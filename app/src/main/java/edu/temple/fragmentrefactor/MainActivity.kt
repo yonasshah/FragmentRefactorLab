da9d03c23af3567008ee2fd4920ee30fa87c26ca
yonasshah <tup36693@temple.edu>
2024-10-14 16:11:45 -0400

    Activity completed

--- app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
@@ -11,18 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val displayTextView = findViewById<TextView>(R.id.displayTextView)
-        val nameEditText = findViewById<EditText>(R.id.nameEditText)
-        val changeButton = findViewById<Button>(R.id.changeButton)
+    val fragmentManager = testFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView, fragmentManager)
+            .commit()
 
-        changeButton.setOnClickListener {
-            val name = nameEditText.text
-
-            displayTextView.text = if (name.isNotBlank()) {
-                "Hello, $name!"
-            } else {
-                "Please enter your name"
-            }
-        }
     }
 }
---
+++ app/src/main/java/edu/temple/fragmentrefactor/testFragment.kt
@@ -0,0 +1,38 @@
+package edu.temple.fragmentrefactor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+
+class testFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val layout = inflater.inflate(R.layout.fragment_test, container, false)
+
+        val displayTextView = layout.findViewById<TextView>(R.id.displayTextView)
+        val nameEditText = layout.findViewById<EditText>(R.id.nameEditText)
+        val changeButton = layout.findViewById<Button>(R.id.changeButton)
+
+        changeButton.setOnClickListener {
+            val name = nameEditText.text
+
+            displayTextView.text = if (name.isNotBlank()) {
+                "Hello, $name!"
+            } else {
+                "Please enter your name"
+            }
+        }
+        return layout
+    }
+
+}
